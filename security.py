4a2350ec861e4e67749ee945a2a5dd663674c95b
chandrakiran112005 <chandrakiran112005@gmail.com>
2026-09-13 10:55:33 +0530

    Update security.py

--- security.py
+++ security.py
@@ -82,13 +82,16 @@ def analyze_password(username, password):
     # -----------------------
     # Username Check
     # -----------------------
-    if username.lower() in password.lower():
-        checks.append(("Username Not Used", False))
-        suggestions.append("Do not include your username in the password.")
-    else:
-        checks.append(("Username Not Used", True))
-        score += 10
-        nist += 10
+   # Username Check
+username_parts = username.lower().split()
+
+if any(part in password.lower() for part in username_parts):
+    checks.append(("Username Not Used", False))
+    suggestions.append("Do not include your username in the password.")
+else:
+    checks.append(("Username Not Used", True))
+    score += 10
+    nist += 10
 
     # -----------------------
     # Common Password
